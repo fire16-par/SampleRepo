9783fcbc763998ee8d6583d399d5666765eddc4c
fire16-par <prasukjainlakheri.2001@gmail.com>
2022-08-02 14:05:24 +0530

    Adding api call Functionality

--- DemoGit/HelloWorld.java
+++ DemoGit/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
 	public void display() {
 		System.out.println("This is a Display Method !!");
 	}
-
+    
+	public void apicall() {
+		System.out.println("calling api");
+	}
 }
